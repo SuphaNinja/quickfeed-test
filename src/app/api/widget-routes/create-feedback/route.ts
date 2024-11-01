6e332044a99ce88801bc14f41ea1c7359d6ca696
SuphaNinja <sidricobjork@gmail.com>
2024-11-01 16:21:50 +0100

    bug isze

--- src/app/api/widget-routes/create-feedback/route.ts
+++ src/app/api/widget-routes/create-feedback/route.ts
@@ -1,14 +1,34 @@
-import { NextResponse } from 'next/server'
+/* import { NextResponse } from 'next/server';
+import { z } from 'zod';
+import { prisma } from '@/lib/prisma';
+
 
 export async function POST(request: Request) {
-  
-
   try {
-   const { description, name, rating, projectId } = await request.json()
+    // Parse and validate the request body
+    const body = await request.json();
 
-   if (!description)
-    
+    // Check if the project exists
+    const project = await prisma.projectRoom.findUnique({
+      where: { id: body.projectId },
+    });
+
+    if (!project) {
+      return NextResponse.json({ error: "Project not found" }, { status: 404 });
+    }
+
+    // Create the feedback
+    const feedback = await prisma.feedback.create({
+      data: {
+
+      },
+    });
+
+
+
+    return NextResponse.json(feedback, { status: 201 });
+
   } catch (error) {
-    return NextResponse.json({ error: 'An unknown error occurred during your request.' }, { status: 500 })
+    return NextResponse.json({ error: 'An unknown error occurred during your request.' }, { status: 500 });
   }
-}
+} */
